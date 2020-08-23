7443901213d0ef23a8db061f328237f841340253
Daniel Vitor Matos <dedomatos12@gmail.com>
2020-08-23 17:25:53 -0300

    Adicionada conexão ao bd e feita a criacao da primeira tabela utilizando migrations

--- CursoMVC/Models/Context.cs
+++ CursoMVC/Models/Context.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CursoMVC.Models
 {
-    public class Context
+    public class Context : DbContext
     {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Integrated Security = SSPI; Persist Security Info = False; Initial Catalog = MeuServer; Data Source =.");
+        } 
+
     }
 }
